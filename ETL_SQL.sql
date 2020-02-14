621f1cbf3e302736394b4f2516b6a456c6ca11a8
Haydenbu <56512655+Haydenbu@users.noreply.github.com>
2020-02-13 19:19:33 -0600

    Add files via upload

--- ETL_SQL.sql
+++ ETL_SQL.sql
@@ -12,13 +12,13 @@ Healthy_life_expectancy DECIMAL
 CREATE TABLE govt_data (
 Country TEXT PRIMARY KEY,
 Units TEXT,
-"2019" DECIMAL
+"2019_govt_spend_to_gdp" DECIMAL
 );
 
 SELECT * FROM govt_data;
 SELECT * FROM happy_data;
 
-SELECT happy_data.overall_rank, happy_data.country, happy_data.score, happy_data.gdp_per_capita, happy_data.social_support, happy_data.healthy_life_expectancy, govt_data.units, govt_data."2019"
+SELECT happy_data.overall_rank, happy_data.country, happy_data.score, happy_data.gdp_per_capita, happy_data.social_support, happy_data.healthy_life_expectancy, govt_data."2019_govt_spend_to_gdp"
 FROM happy_data
 JOIN govt_data
 ON happy_data.country = govt_data.country;
