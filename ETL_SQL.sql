5ffb78b3a802df7f54056a6617055ea40ee1d50b
Haydenbu <56512655+Haydenbu@users.noreply.github.com>
2020-02-13 18:45:07 -0600

    Add files via upload

---
+++ ETL_SQL.sql
@@ -0,0 +1,24 @@
+DROP TABLE happy_data,govt_data;
+
+CREATE TABLE happy_data (
+Overall_rank INT,
+Country TEXT PRIMARY KEY,
+Score DECIMAL,
+GDP_per_capita DECIMAL,
+Social_support DECIMAL,
+Healthy_life_expectancy DECIMAL
+);
+
+CREATE TABLE govt_data (
+Country TEXT PRIMARY KEY,
+Units TEXT,
+"2019" DECIMAL
+);
+
+SELECT * FROM govt_data;
+SELECT * FROM happy_data;
+
+SELECT happy_data.overall_rank, happy_data.country, happy_data.score, happy_data.gdp_per_capita, happy_data.social_support, happy_data.healthy_life_expectancy, govt_data.units, govt_data."2019"
+FROM happy_data
+JOIN govt_data
+ON happy_data.country = govt_data.country;
